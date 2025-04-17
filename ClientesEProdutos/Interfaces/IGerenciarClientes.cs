a9bd6a7102dfb9f4c107f0ea815471ec3d68841a
lucas-bubanz <lucas.bubanz@herval.com.br>
2025-04-17 08:26:34 -0300

    ✨ Implementado os métodos assíncronos na função de "CadastrarNovoCliente" para uma melhor perfomance e divisão de tarefas/threads na aplicação.

--- ClientesEProdutos/Interfaces/IGerenciarClientes.cs
+++ ClientesEProdutos/Interfaces/IGerenciarClientes.cs
@@ -2,7 +2,7 @@ namespace ClientesEProdutos.Interfaces.IGerenciarClientes
 {
     public interface IGerenciarClientes
     {
-        void CadastrarNovoCliente(Models.Clientes.Clientes clientes);
+        Task CadastrarNovoCliente(Models.Clientes.Clientes clientes);
         void ListarClientes();
         void RemoverClientes();
         void AtualizarClietnes();
