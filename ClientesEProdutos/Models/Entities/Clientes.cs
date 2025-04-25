1b3c5725f57258cb13d427e50d612457dfb03f02
lucas-bubanz <lucas.bubanz@herval.com.br>
2025-04-25 16:37:39 -0300

    🔧 Adicionados atributos de mapeamento para as entidades Clientes e Produtos, garantindo a correta associação das propriedades com as colunas do banco de dados.

--- ClientesEProdutos/Models/Entities/Clientes.cs
+++ ClientesEProdutos/Models/Entities/Clientes.cs
@@ -1,13 +1,20 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ClientesEProdutos.Models.Entities
 {
     public class Clientes
     {
         [Key]
+        [Column("codigo_cliente")]
         public int Codigo_cliente { get; set; }
+
+
         [Required]
+        [Column("nome_cliente")]
         public string Nome_cliente { get; set; }
+
+        [Column("endereco_cliente")]
         public string Endereco_cliente { get; set; }
     }
 }
--- ClientesEProdutos/Models/Entities/Produtos.cs
+++ ClientesEProdutos/Models/Entities/Produtos.cs
@@ -7,11 +7,17 @@ namespace ClientesEProdutos.Models.Entities
     public class Produtos
     {
         [Key]
+        [Column("codigo_produto")]
         public int Codigo_produto { get; set; }
+
         [Required]
+        [Column("nome_produto")]
         public string Nome_produto { get; set; }
+
         [Required]
+        [Column("valor_produto")]
         public decimal ValorProduto { get; set; }
+
         [Column("descricao_produto")]
         public string DescricaoProduto { get; set; }
     }
