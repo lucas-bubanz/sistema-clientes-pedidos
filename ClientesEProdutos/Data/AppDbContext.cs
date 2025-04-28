c9cf33a6b5b4c0549fab416680464fae06128826
lucas-bubanz <lucas.bubanz@herval.com.br>
2025-04-28 09:17:28 -0300

    🔨 Criado a entidade Pré Pedido com a relação de Foreign Key para entidade Cliente e Produtos.

--- ClientesEProdutos/Data/AppDbContext.cs
+++ ClientesEProdutos/Data/AppDbContext.cs
@@ -16,6 +16,7 @@ namespace ApplicationDBContext.Data
 
         public DbSet<Clientes> clientes { get; set; }
         public DbSet<Produtos> produtos { get; set; }
+        public DbSet<PrePedido> PrePedidos { get; set; }
 
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
@@ -24,7 +25,26 @@ namespace ApplicationDBContext.Data
             {
                 var connectionString = _configuration.GetConnectionString("DefaultConnection");
                 optionsBuilder.UseNpgsql(connectionString);
+
             }
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PrePedido>().ToTable("pre_pedidos");
+
+            // Configurar relação PrePedido -> Clientes
+            modelBuilder.Entity<PrePedido>()
+                .HasOne(pp => pp.Cliente)
+                .WithMany()
+                .HasForeignKey(pp => pp.CodigoCliente)
+                .OnDelete(DeleteBehavior.Restrict); // Evita exclusão em cascata
+
+            // Configurar relação PrePedido -> Produtos
+            modelBuilder.Entity<PrePedido>()
+                .HasOne(pp => pp.Produto)
+                .WithMany()
+                .HasForeignKey(pp => pp.CodigoProduto)
+                .OnDelete(DeleteBehavior.Restrict);
         }
     }
 }
---
+++ ClientesEProdutos/Models/Entities/PrePedido.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClientesEProdutos.Models.Entities
+{
+    public class PrePedido
+    {
+        [Key]
+        [Column("id_pre_pedido")]
+        public int IdPrePedido { get; set; }
+
+        // Informações do Cliente        
+        [Required]
+        [Column("codigo_cliente")]
+        public int CodigoCliente { get; set; }
+        public Clientes Cliente { get; set; } // Chave estrangeira para Cliente
+
+        [Required]
+        [Column("nome_cliente")]
+        public string NomeCliente { get; set; }
+
+        [Required]
+        [Column("endereco_cliente")]
+        public string EnderecoCliente { get; set; }
+
+        // Informações do Produto
+        [Required]
+        [Column("codigo_produto")]
+        public int CodigoProduto { get; set; }
+        public Produtos Produto { get; set; } // Chave estrangeira para Produto
+
+        [Required]
+        [Column("nome_produto")]
+        public string NomeProduto { get; set; }
+
+        [Required]
+        [Column("valor_produto")]
+        public decimal ValorProduto { get; set; }
+
+        [Column("descricao_produto")]
+        public string DescricaoProduto { get; set; }
+    }
+}
